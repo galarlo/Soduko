c1cb0b70076104ad01a9a3fd0c4b7f287600d879
Gal Arlazoroff <galgol.gal800@gmail.com>
2022-01-15 12:39:27 +0200

    add css class for clues

--- src/SodukoCell.js
+++ src/SodukoCell.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 function SodukoCell(props) {
     const [isClue, _] = useState(props.value != " ")
 
-    return <div className='sodukoCell'>
+    return <div className={'sodukoCell ' + (isClue ? "clueCell" : "")}>
         <input type="number" value={props.value} disabled={isClue} min={1} max={9} 
             onChange={(e)=>props.onChange(props.row, props.column, e.target.value)} />
     </div>
